41c685c2358ec99e0fa6b84e4a7f5a2685ed1215
Breno Tainan Aguiar <brenotaguiar@gmail.com>
2024-07-14 21:02:48 -0300

    mecanismo de lazy loading

--- lib/features/home/widgets/news_list.dart
+++ lib/features/home/widgets/news_list.dart
@@ -11,37 +11,79 @@ class NewsList extends StatefulWidget {
 }
 
 class _NewsListState extends State<NewsList> {
-  List<dynamic> newsItems = [];
+  static const int pageSize = 2;
+  List<dynamic> loadedNewsItems = [];
+  bool _hasMore = true;
+  bool _isLoading = false;
 
   @override
   void initState() {
     super.initState();
-    _loadNewsItems();
+    _loadInitialData();
   }
 
-  Future<void> _loadNewsItems() async {
+  Future<void> _loadInitialData() async {
+    _isLoading = true;
     final jsonString = await rootBundle.loadString('assets/json/news.json');
     final jsonResponse = json.decode(jsonString);
-    setState(() {
-      newsItems = jsonResponse['newsItems'];
-    });
+    loadedNewsItems.addAll(jsonResponse['newsItems'].sublist(0, pageSize));
+    _isLoading = false;
+    setState(() {});
+  }
+
+  Future<void> _loadMoreData() async {
+    if (!_hasMore || _isLoading) return;
+
+    _isLoading = true;
+    final jsonString = await rootBundle.loadString('assets/json/news.json');
+    final jsonResponse = json.decode(jsonString);
+    final totalItems = jsonResponse['newsItems'].length;
+    final int nextIndex = loadedNewsItems.length + pageSize;
+    // Garante que o índice final não exceda o tamanho total da lista
+    final int endIndex = nextIndex > totalItems ? totalItems : nextIndex;
+
+    List<dynamic> nextItems =
+        jsonResponse['newsItems'].sublist(loadedNewsItems.length, endIndex);
+
+    if (nextItems.isEmpty) {
+      _hasMore = false;
+    } else {
+      // Carrega e exibe cada item individualmente com um atraso
+      for (var item in nextItems) {
+        loadedNewsItems.add(item);
+        await Future.delayed(const Duration(milliseconds: 1000));
+        setState(() {});
+      }
+    }
+
+    _isLoading = false;
   }
 
   @override
   Widget build(BuildContext context) {
     return SliverList(
       delegate: SliverChildBuilderDelegate(
-        (context, i) => NewsListItem(
-          idNews: newsItems[i]['idNews']!,
-          imageAssetPath: newsItems[i]['imageAssetPath']!,
-          category: newsItems[i]['category']!,
-          title: newsItems[i]['title']!,
-          content: newsItems[i]['content']!,
-          author: newsItems[i]['author']!,
-          authorImageAssetPath: newsItems[i]['authorImageAssetPath']!,
-          date: DateTime.parse(newsItems[i]['date']!),
-        ),
-        childCount: newsItems.length,
+        (context, index) {
+          if (index == loadedNewsItems.length) {
+            if (_hasMore) {
+              _loadMoreData();
+              return const Center(child: CircularProgressIndicator());
+            }
+            return null;
+          }
+          final newsItem = loadedNewsItems[index];
+          return NewsListItem(
+            idNews: newsItem['idNews']!,
+            imageAssetPath: newsItem['imageAssetPath']!,
+            category: newsItem['category']!,
+            title: newsItem['title']!,
+            content: newsItem['content']!,
+            author: newsItem['author']!,
+            authorImageAssetPath: newsItem['authorImageAssetPath']!,
+            date: DateTime.parse(newsItem['date']!),
+          );
+        },
+        childCount: loadedNewsItems.length + (_hasMore ? 1 : 0),
       ),
     );
   }
